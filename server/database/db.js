fceefa597066ba51486bc7f142fd8df3a4ef831d
ShahuKor <korshahu17@gmail.com>
2026-01-18 14:18:05 +0000

    Add retryconnection logic

--- server/database/db.js
+++ server/database/db.js
@@ -8,38 +8,103 @@ class DatabaseConnection {
     this.retryCount = 0;
     this.isConnected = false;
     mongoose.set("strictQuery", true);
+
     mongoose.connection.on("connected", () => {
       console.log("MongoDB connected Successfully");
       this.isConnected = true;
     });
+
     mongoose.connection.on("error", () => {
       console.log("MongoDB connection Error");
       this.isConnected = false;
     });
+
     mongoose.connection.on("disconnected", () => {
       console.log("MongoDB disconnected");
       this.isConnected = false;
+      this.retryConnection();
     });
+
+    process.on("SIGTERM", this.handleAppTermination.bind(this));
   }
 
   async connect() {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MongoDb URI is not defined in the env file");
-    }
+    try {
+      if (!process.env.MONGO_URI) {
+        throw new Error("MongoDb URI is not defined in the env file");
+      }
 
-    const connectionOptions = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      maxPoolSize: 10,
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 4500,
-      family: 4,
+      const connectionOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        maxPoolSize: 10,
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 4500,
+        family: 4,
+      };
+
+      if (process.env.NODE_ENV === "development") {
+        mongoose.set("debug", true);
+      }
+
+      await mongoose.connect(process.env.MONGO_URI, connectionOptions);
+      this.retryCount = 0;
+    } catch (error) {
+      console.error(error.message);
+      await this.handleConnetionError();
+    }
+  }
+
+  async handleConnetionError() {
+    if (this.retryCount < MAX_RETRIES) {
+      this.retryCount++;
+      console.log(
+        `Retrying the connection, Attempt ${this.retryCount} / ${MAX_RETRIES}`,
+      );
+      await new Promise((resolve) => setTimeout({ resolve }, MAX_RETRIES));
+
+      return this.connect();
+    } else {
+      console.error(
+        `Failed to connect to Mongodb after ${MAX_RETRIES} attempts`,
+      );
+      process.exit(1);
+    }
+  }
+
+  async retryConnection() {
+    if (!this.isConnected) {
+      console.log("Attempting to reconnect to mongodb");
+      this.connect();
+    }
+  }
+
+  async handleAppTermination() {
+    try {
+      await mongoose.connection.close();
+      console.log("MongoDB connection closed through app termination");
+      process.exit(0);
+    } catch (error) {
+      console.error(
+        "Error during disconnecting the database through app Termination error : ",
+        error,
+      );
+      process.exit(1);
+    }
+  }
+
+  getConnectionStatus() {
+    return {
+      isConnected: this.isConnected,
+      readyState: mongoose.connection.readyState,
+      hostName: mongoose.connection.host,
     };
-
-    if (process.env.NODE_ENV === "development") {
-      mongoose.set("debug", true);
-    }
-
-    await mongoose.connect(process.env.MONGO_URI, connectionOptions);
   }
 }
+
+// create singleton object
+
+const dbConnection = new DatabaseConnection();
+
+export default dbConnection.connection.bind(dbConnection);
+export const getDbStatus = dbConnection.getConnectionStatus.bind(dbConnection);
